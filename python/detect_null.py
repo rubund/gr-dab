9b1adc7db80f919a556795bc7a059e6d821d8ed8
Ruben Undheim <ruben.undheim@gmail.com>
2015-02-07 19:06:30 +0100

    Small fixes

--- python/detect_null.py
+++ python/detect_null.py
@@ -53,10 +53,10 @@ class detect_null(gr.hier_block2):
 		#self.ns_filter = gr.iir_filter_ffd([1]+[0]*(length-1)+[-1],[0,1])
 		# this does the same again, but is actually faster (outsourced to an independent block ..):
 		self.ns_moving_sum = dab.moving_sum_ff(length)
-		self.ns_invert = gr.multiply_const_ff(-1)
+		self.ns_invert = blocks.multiply_const_ff(-1)
 
 		# peak detector on the inverted, summed up signal -> we get the nulls (i.e. the position of the start of a frame)
-		self.ns_peak_detect = gr.peak_detector_fb(0.6,0.7,10,0.0001) # mostly found by try and error -> remember that the values are negative!
+		self.ns_peak_detect = blocks.peak_detector_fb(0.6,0.7,10,0.0001) # mostly found by try and error -> remember that the values are negative!
 
 		# connect it all
 		self.connect(self, self.ns_c2magsquared, self.ns_moving_sum, self.ns_invert, self.ns_peak_detect, self)
