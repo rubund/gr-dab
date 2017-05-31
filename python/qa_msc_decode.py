6d78462879ce14da082e0e85a8dcec21515c8bb6
MoritzLucaSchmid <luca.moritz.schmid@gmail.com>
2017-05-31 15:56:31 +0200

    msc_decode: fix subchannel length for reference data

--- python/qa_msc_decode.py
+++ python/qa_msc_decode.py
@@ -36,7 +36,7 @@ class qa_msc_decode (gr_unittest.TestCase):
         self.dab_params = dab.parameters.dab_parameters(1 , 208.064e6, True)
         self.src01 = blocks.file_source_make(gr.sizeof_float * 2*self.dab_params.num_carriers, "debug/transmission_frame.dat")
         self.src02 = blocks.file_source_make(gr.sizeof_char, "debug/transmission_frame_trigger.dat")
-        self.msc = dab.msc_decode(self.dab_params, 54, 84, 2, 1, 1)
+        self.msc = dab.msc_decode(self.dab_params, 54, 90, 2, 1, 1)
 
         self.tb.connect(self.src01, (self.msc, 0), blocks.null_sink_make(gr.sizeof_char))
         self.tb.connect(self.src02, (self.msc, 1))
