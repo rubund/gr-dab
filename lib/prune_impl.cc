6d1ac587b28eec242b5dd9f425bb4e1376f43a01
MoritzLucaSchmid <luca.moritz.schmid@gmail.com>
2017-06-07 08:43:21 +0200

    change assert to exception

--- lib/prune_impl.cc
+++ lib/prune_impl.cc
@@ -30,71 +30,65 @@
 #include <boost/format.hpp>
 
 namespace gr {
-  namespace dab {
+    namespace dab {
 
-    prune::sptr
-    prune::make(size_t itemsize, unsigned int length, unsigned int prune_start, unsigned int prune_end)
-    {
-      return gnuradio::get_initial_sptr
-        (new prune_impl(itemsize, length, prune_start, prune_end));
-    }
-
-    /*
-     * The private constructor
-     */
-    prune_impl::prune_impl(size_t itemsize, unsigned int length, unsigned int prune_start, unsigned int prune_end)
-      : gr::block("prune",
-              gr::io_signature::make(1, 1, sizeof(char)),
-              gr::io_signature::make(1, 1, sizeof(char))),
-      d_itemsize(itemsize), d_length(length), d_prune_start(prune_start), d_prune_end(prune_end)
-    {
-        try
+        prune::sptr
+        prune::make(size_t itemsize, unsigned int length, unsigned int prune_start, unsigned int prune_end)
         {
-            if (prune_start+prune_end > length) throw (prune_start + prune_end - length);
+            return gnuradio::get_initial_sptr
+                    (new prune_impl(itemsize, length, prune_start, prune_end));
         }
-        catch (int overprune)
+
+        /*
+         * The private constructor
+         */
+        prune_impl::prune_impl(size_t itemsize, unsigned int length, unsigned int prune_start, unsigned int prune_end)
+                : gr::block("prune",
+                            gr::io_signature::make(1, 1, sizeof(char)),
+                            gr::io_signature::make(1, 1, sizeof(char))),
+                  d_itemsize(itemsize), d_length(length), d_prune_start(prune_start), d_prune_end(prune_end)
         {
-            GR_LOG_WARN(d_logger, "want to cut more items than stream is long");
+            if (prune_start + prune_end > length)
+                throw fprintf(stderr, "want to cut %d more items than stream is long", prune_start + prune_end - length);
+
+            set_output_multiple(length - prune_start - prune_end);
+            set_relative_rate((length - prune_start - prune_end) / length);
         }
 
-        set_output_multiple(length - prune_start - prune_end);
-        set_relative_rate((length - prune_start - prune_end) / length);
-    }
-
-    /*
-     * Our virtual destructor.
-     */
-    prune_impl::~prune_impl()
-    {
-    }
-
-    void
-    prune_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
-    {
-      ninput_items_required[0] = (noutput_items/(d_length - d_prune_start - d_prune_end))*d_length;
-    }
-
-    int
-    prune_impl::general_work (int noutput_items,
-                       gr_vector_int &ninput_items,
-                       gr_vector_const_void_star &input_items,
-                       gr_vector_void_star &output_items)
-    {
-      const char *in = (const char *) input_items[0];
-      char *out = (char *) output_items[0];
-        for(int i = 0; i < noutput_items/(d_length - d_prune_start - d_prune_end); i++){
-            memcpy(out,in+d_prune_start*d_itemsize,(d_length-d_prune_start-d_prune_end)*d_itemsize);
-            in  += d_length*d_itemsize;
-            out += (d_length-d_prune_start-d_prune_end)*d_itemsize;
+        /*
+         * Our virtual destructor.
+         */
+        prune_impl::~prune_impl()
+        {
         }
-      // Tell runtime system how many input items we consumed on
-      // each input stream.
-      consume_each ((noutput_items/(d_length - d_prune_start - d_prune_end))*d_length);
 
-      // Tell runtime system how many output items we produced.
-      return noutput_items;
-    }
+        void
+        prune_impl::forecast(int noutput_items, gr_vector_int &ninput_items_required)
+        {
+            ninput_items_required[0] = (noutput_items / (d_length - d_prune_start - d_prune_end)) * d_length;
+        }
 
-  } /* namespace dab */
+        int
+        prune_impl::general_work(int noutput_items,
+                                 gr_vector_int &ninput_items,
+                                 gr_vector_const_void_star &input_items,
+                                 gr_vector_void_star &output_items)
+        {
+            const char *in = (const char *) input_items[0];
+            char *out = (char *) output_items[0];
+            for (int i = 0; i < noutput_items / (d_length - d_prune_start - d_prune_end); i++) {
+                memcpy(out, in + d_prune_start * d_itemsize, (d_length - d_prune_start - d_prune_end) * d_itemsize);
+                in += d_length * d_itemsize;
+                out += (d_length - d_prune_start - d_prune_end) * d_itemsize;
+            }
+            // Tell runtime system how many input items we consumed on
+            // each input stream.
+            consume_each((noutput_items / (d_length - d_prune_start - d_prune_end)) * d_length);
+
+            // Tell runtime system how many output items we produced.
+            return noutput_items;
+        }
+
+    } /* namespace dab */
 } /* namespace gr */
 
