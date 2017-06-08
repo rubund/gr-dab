fcd78a577fc6564f0e1c42857b91553e3d813ccb
MoritzLucaSchmid <luca.moritz.schmid@gmail.com>
2017-06-08 12:09:08 +0200

    transmission: add loopback test

--- apps/usrp_dab_tx.py
+++ apps/usrp_dab_tx.py
@@ -19,8 +19,8 @@ import sys
 class usrp_dab_tx(gr.top_block):
 	def __init__(self):
 		gr.top_block.__init__(self)
-        
-		
+
+
 		parser = OptionParser(option_class=eng_option, usage="%prog: [options] input-filename")
 		parser.add_option("-T", "--tx-subdev-spec", type="subdev", default=(0, 0),
 		     help="select USRP Tx side A or B [default=A]")
@@ -49,10 +49,10 @@ class usrp_dab_tx(gr.top_block):
 
 		self.src = blocks.file_source(gr.sizeof_char, self.filename)
 		self.trigsrc = blocks.vector_source_b([1]+[0]*(self.dab_params.symbols_per_frame-1),True)
-		
+
 		self.s2v = blocks.stream_to_vector(gr.sizeof_char, 384)
-		
-		self.mod = dab.ofdm_mod(self.dab_params, verbose=options.verbose) 
+
+		self.mod = dab.ofdm_mod(self.dab_params, verbose=options.verbose)
 
 		#self.sink = usrp.sink_c(interp_rate = interp)
 		self.sink = uhd.usrp_sink("",uhd.io_type.COMPLEX_FLOAT32,1)
@@ -60,7 +60,7 @@ class usrp_dab_tx(gr.top_block):
         	#self.sink.set_mux(usrp.determine_tx_mux_value(self.sink, options.tx_subdev_spec))
         	#self.subdev = usrp.selected_subdev(self.sink, options.tx_subdev_spec)
 		self.sink.set_antenna(options.antenna)
-		
+
 		print "--> using sample rate: " + str(self.sample_rate)
 
 
@@ -70,7 +70,7 @@ class usrp_dab_tx(gr.top_block):
 		# tune frequency
 		self.sink.set_center_freq(options.freq)
 
-		# set gain      
+		# set gain
 		if options.tx_gain is None:
 			# if no gain was specified, use the mid-point in dB
 			g = self.sink.get_gain_range()
