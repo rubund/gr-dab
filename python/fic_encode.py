a88d555840a3ecfd880a4197a99d5ab0267b82e9
MoritzLucaSchmid <luca.moritz.schmid@gmail.com>
2017-06-21 10:08:41 +0200

    transmitter: fic_encoder with qa_test passed

--- python/fic_encode.py
+++ python/fic_encode.py
@@ -54,9 +54,6 @@ class fic_encode(gr.hier_block2):
         self.add_mod_2 = blocks.xor_bb()
 
         # convolutional encoder
-        #self.append_zeros = dab.append_bb_make(768, self.dp.energy_dispersal_fic_vector_length + self.dp.conv_code_add_bits_input)
-        #self.conv_encoder_config = fec.cc_encoder_make(self.dp.energy_dispersal_fic_vector_length, 7, 4, [91, 121, 101, 91], 0, fec.CC_TERMINATED)
-        #self.conv_encoder = fec.extended_encoder(self.conv_encoder_config, None, "1")
         self.conv_pack = blocks.unpacked_to_packed_bb_make(1, gr.GR_MSB_FIRST)
         self.conv_encoder = dab.conv_encoder_bb_make(self.dp.energy_dispersal_fic_vector_length/8)
         self.conv_unpack = blocks.packed_to_unpacked_bb_make(1, gr.GR_MSB_FIRST)
@@ -76,7 +73,6 @@ class fic_encode(gr.hier_block2):
                      self.v2s_crc,
                      self.packed_to_unpacked_crc,
                      (self.add_mod_2, 0),
-                     #self.append_zeros,
                      self.conv_pack,
                      self.conv_encoder,
                      self.conv_unpack,
--- python/qa_fic_encode.py
+++ python/qa_fic_encode.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 
-# Copyright 2017 <+YOU OR YOUR COMPANY+>.
+# Copyright 2017 Moritz Luca Schmid, Communications Engineering Lab (CEL) / Karlsruhe Institute of Technology (KIT).
 # 
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -32,55 +32,46 @@ class qa_fic_encode (gr_unittest.TestCase):
     def tearDown (self):
         self.tb = None
 
+# loopback test: fib_source - fic_encoder - mapper - demapper - fic_decoder - fib_sink
+# manual check, if fibs are correctly processed through the coding and mapping blocks
     def test_001_t (self):
         self.symbol_length = 32*4
         self.dab_params = dab.parameters.dab_parameters(1, 208.064e6, True)
 
         # source
         self.dp = dab.parameters.dab_parameters(1, 208.064e6, True)
-        self.fib_src = dab.fib_source_b_make(1, 1, "ensemble1", "service1", "bla", 4, 2, 15)
-        #self.source = blocks.file_source_make(gr.sizeof_gr_complex, "debug/170524/ofdm_deinterleaved.dat")
+        self.fib_src = dab.fib_source_b_make(1, 1, "ensemble1", "service1", "musicmix", 4, 2, 15)
 
         # encoder
         self.fib_enc = dab.fic_encode(self.dab_params)
+        self.unpack = blocks.packed_to_unpacked_bb_make(1, gr.GR_MSB_FIRST)
 
         # mapper
-        self.s2v_map = blocks.stream_to_vector_make(gr.sizeof_char, self.dp.num_carriers/4)
-        self.map = dab.qpsk_mapper_vbc_make(self.dp.num_carriers)
-        #self.v2s_map = blocks.vector_to_stream_make(gr.sizeof_gr_complex, self.dp.fic_punctured_codeword_length*4)
+        self.map = dab.mapper_bc_make(self.dp.num_carriers)
 
         # demapper
+        self.s2v = blocks.stream_to_vector_make(gr.sizeof_gr_complex, self.dp.num_carriers)
         self.soft_interleaver = dab.complex_to_interleaved_float_vcf_make(self.dp.num_carriers)
 
         # decode
         self.fic_decoder = dab.fic_decode(self.dab_params)
 
         # control stream
-        self.trigger_src = blocks.vector_source_b([1] + [0]*(74), True)
-
-
-
-
-
-
-        #self.file_sink = blocks.file_sink_make(gr.sizeof_gr_complex, "debug/170524/.dat")
-        self.sink = blocks.vector_sink_c()
+        self.trigger_src = blocks.vector_source_b([1] + [0]*74, True)
 
         self.tb.connect(self.fib_src,
-                        blocks.head_make(gr.sizeof_char, 10000000),
+                        blocks.head_make(gr.sizeof_char, 100000),
                         self.fib_enc,
-                        self.s2v_map,
+                        self.unpack,
                         self.map,
-                        #self.v2s_map,
+                        self.s2v,
                         self.soft_interleaver,
-                        self.fic_decoder)
+                        self.fic_decoder
+                        )
         self.tb.connect(self.trigger_src, (self.fic_decoder, 1))
 
 
         self.tb.run ()
-        result = self.sink.data()
-        for item in result:
-           print(item)
         pass
 
 
